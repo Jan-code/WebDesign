f58c30eaa8211c5261134555afab3325b65d7c6d
Jan-code <839234349@qq.com>
2020-04-23 20:58:41 +0800

    frontend add Home.vue-page/MyFooter-component

---
+++ frontend/src/components/MyFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-footer>
+    <v-container fluid>
+      <v-row>
+        <v-col md="2" offset-md="1">
+          <div class="font-weight-bold body-1">咨询</div>
+          <div class="font-weight-light body-2">平台介绍</div>
+          <div class="font-weight-light body-2">帮助中心</div>
+        </v-col>
+        <v-col md="2">
+          <div class="font-weight-bold body-1">推广</div>
+          <div class="font-weight-light body-2">招商合作</div>
+          <div class="font-weight-light body-2">业务推荐</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col offset-md="1">
+          <div class="font-weight-light caption">
+            Copyright &copy; Jan-Code {{ new Date().getFullYear() }}
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+  data: () => ({})
+};
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-img
+      :aspect-ratio="16 / 9"
+      src="@/../public/home.jpg"
+      class="white--text"
+    >
+      <v-container style="height: 100%">
+        <v-row class="py-3">
+          <div class="headline font-italic">Jan-Code & VD</div>
+        </v-row>
+        <v-spacer style="height: 30%"></v-spacer>
+        <v-row class="py-3 px-11">
+          <div class="display-3">全栈开发之路</div>
+        </v-row>
+        <v-row class="py-3 px-12">
+          <div class="body-1">Vue-Vuetify / Node-Express</div>
+        </v-row>
+        <v-row class="py-12 px-10">
+          <v-col md="2">
+            <v-btn rounded color="#004D40" width="120" class="white--text"
+              >合作入口
+            </v-btn>
+          </v-col>
+          <v-col md="2">
+            <v-btn rounded color="#004D40" width="120" class="white--text"
+              >用户入口
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-img>
+
+    <my-footer />
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    MyFooter: () => import("@/components/MyFooter")
+  },
+  data: () => ({})
 };
 </script>
